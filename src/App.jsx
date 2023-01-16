624ba2e188368671df76d202f274302f6d1278fa
Finnvb <finnvb@gmail.com>
2023-01-16 15:43:12 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {BrowserRouter as Router, Route, Routes, useParams } from "react-router-dom";
+
+import HomePage from "./pages/homepage";
+// import OverviewPage from "./pages/Overview";
+// import UidPage from "./pages/Uid";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Router>
+ <div>
+    {/* <Switch>
+      <Route path='/' exact>
+        <HomePage/>
+      </Route>
+      <Route path='/overview'>
+       <HomePage/>
+      </Route>
+      <Route path='/:uid'>
+      <HomePage/>
+      </Route>
+    </Switch> */}
+
+    <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/overview" element={<HomePage />} />
+      </Routes>
+ </div>
+ </Router>
+  );
 }
 
-export default App
+export default App;
