4de74f4d4b433c80d3dc3b124a79f9fd55bf633d
Finnvb <finnvb@gmail.com>
2023-01-18 16:13:05 +0100

    uid route added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Route, Routes, useParams } from "react-router-d
 import HomePage from "./pages/homepage";
 import Test from "./pages/Test";
 // import OverviewPage from "./pages/Overview";
-// import UidPage from "./pages/Uid";
+import UidPage from "./pages/Uid";
 
 function App() {
   return (
@@ -25,6 +25,9 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/overview" element={<HomePage />} />
         <Route path="/test" element={<Test />} />
+        <Route path='/:uid' element={<UidPage />}/>
+      
+      
       </Routes>
  </div>
  </Router>
---
+++ src/pages/Uid.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+import './Uid.css'
+
+export default function Uidpage() {
+
+return(
+    <main>
+<Navbar/>
+
+<p className="test">hgtreiisntgdijn</p>
+<Footer/>
+</main>
+)
+}
+
